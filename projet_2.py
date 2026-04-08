ce1c0cfd8db591e1ba6d66d024c4ccd77054e0b6
Marc45387 <marc.chen@edu.univ-eiffel.fr>
2026-04-08 11:58:04 +0200

    Add files via upload

--- projet_2.py
+++ projet_2.py
@@ -1,19 +1,29 @@
 from fltk import * 
 
 class Interface:
-    def __init__(self) -> None:
+    def __init__(self):
         self.HAUTEUR = 400 
         self.LONGUEUR = 400 
     
     def page_garde(self):
         efface_tout 
+        
         x, y  = self.LONGUEUR, self.HAUTEUR
-        rectangle(x,y,x - 400,y - 400)
-        ferme_fenetre()
+
+        texte(x -220,40,'Saute Mouton', taille = 30)
+        
+        rectangle(200,200,400,150, couleur = 'black', remplissage = 'white')
+        texte(220,160,'commencer')
+
+        rectangle(200,300,400,250, couleur = 'black', remplissage = 'white')
+        texte(210,260,'Règle du jeu')
+        attend_ev()
 
     def run(self):
-        x, y  = self.LONGUEUR, self.HAUTEUR  
+        x, y  = self.LONGUEUR * 1.5, self.HAUTEUR  
         cree_fenetre(x,y)
         self.page_garde()
 
-Interface.run()
+
+interface = Interface() 
+interface.run()
